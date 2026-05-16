c0edb5340c0515835168ae57ed49696894c81b0a
yoo <77600778+yjgeno@users.noreply.github.com>
2026-05-15 21:05:21 -0400

    Refactor: packaging, logging, CLI (#10)
    Co-authored-by: yjyang <yjyang027@email.com>

--- scTenifoldXct/__init__.py
+++ scTenifoldXct/__init__.py
@@ -1,4 +1,16 @@
-from .version import __version__ # scTenifoldXct.__version__
+import logging
+
+from .version import __version__
 from scTenifoldXct.core import scTenifoldXct
 from scTenifoldXct.visualization import get_Xct_pairs, plot_XNet
 from scTenifoldXct.merge import merge_scTenifoldXct
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = [
+    "__version__",
+    "scTenifoldXct",
+    "merge_scTenifoldXct",
+    "get_Xct_pairs",
+    "plot_XNet",
+]
