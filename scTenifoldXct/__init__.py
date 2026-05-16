78e62a2aab00c2738fabdde890d435e7712c60f3
yjyang <yjyang027@email.com>
2026-05-16 14:11:51 -0400

    fix seeds

--- scTenifoldXct/__init__.py
+++ scTenifoldXct/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from scTenifoldXct.core import scTenifoldXct
 from scTenifoldXct.merge import merge_scTenifoldXct
+from scTenifoldXct.nn import set_seed
 from scTenifoldXct.visualization import get_Xct_pairs, plot_XNet
 
 from .version import __version__
@@ -12,6 +13,7 @@ __all__ = [
     "__version__",
     "scTenifoldXct",
     "merge_scTenifoldXct",
+    "set_seed",
     "get_Xct_pairs",
     "plot_XNet",
 ]
